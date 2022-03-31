53e727e68755e809320c417dc06c035b683f0208
divine-alt <77685066+divine-alt@users.noreply.github.com>
2022-03-31 10:19:10 +0530

    get, post, delete using postman and use of crud repository for database

---
+++ First Spring Project/src/main/java/com/firstspringproject/course/CourseController.java
@@ -0,0 +1,34 @@
+package com.firstspringproject.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id){
+        return courseService.getAllCourses(id);
+    }
+    @RequestMapping("/topics/{topicId}/courses/{courseId}")
+    public Optional<Course> getCourse(@PathVariable String id) {
+        return courseService.getCourse(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course){
+        courseService.addCourse(course);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String id){
+        courseService.updateCourse(id, course);
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "topics/{topicId}/courses/{id}")
+    public void deleteCourse(@PathVariable String id){
+        courseService.deleteCourse(id);
+    }
+}
